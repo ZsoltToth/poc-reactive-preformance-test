101ee79fdadfb197562afd8ea7749240b61dbdfa
Zsolt Toth <tothzs87@gmail.com>
2025-07-27 08:28:36 +0200

    chore: initial commit

---
+++ src/main/java/eu/coeus/poc/performance/reactive/ReactivePerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package eu.coeus.poc.performance.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivePerformanceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/coeus/poc/performance/reactive/ReactivePerformanceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.coeus.poc.performance.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePerformanceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
